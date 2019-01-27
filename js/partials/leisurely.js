1c34f16007e45c61c480ca1670eb4b35064744d0
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-01-27 13:00:12 +0100

    add basic lazyload

---
+++ js/partials/leisurely.js
@@ -0,0 +1,27 @@
+var leisurely = function(options) {
+    // parsing options
+    var defaults = {
+        elements: null,
+        type: 'image',
+        interval: 100
+    };
+    var options = Object.assign(defaults, options);
+
+    // set image src
+    function setSrc(element) {
+        element.src = element.getAttribute("data-src");
+        element.addEventListener("load", function (event) {
+            loadNext();
+        });
+    }
+
+    // iterate through elements
+    var i = 0;
+    var loadNext = function() {
+        if (i < options.elements.length) {
+            setSrc(options.elements[i]);
+            i++;
+        }
+    };
+    loadNext();
+};
--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,33 @@ $.fn.inView = function (options) {
         update();
     });
 };
+var leisurely = function(options) {
+    // parsing options
+    var defaults = {
+        elements: null,
+        type: 'image',
+        interval: 100
+    };
+    var options = Object.assign(defaults, options);
+
+    // set image src
+    function setSrc(element) {
+        element.src = element.getAttribute("data-src");
+        element.addEventListener("load", function (event) {
+            loadNext();
+        });
+    }
+
+    // iterate through elements
+    var i = 0;
+    var loadNext = function() {
+        if (i < options.elements.length) {
+            setSrc(options.elements[i]);
+            i++;
+        }
+    };
+    loadNext();
+};
 $.fn.materializeInputs = function(selectors) {
 
     // default param with backwards compatibility
